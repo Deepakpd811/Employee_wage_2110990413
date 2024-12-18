70460f88ffad8523007bca67936411788f2a7b49
Deepakpd811 <deepak0413.be21@chitkara.edu.in>
2024-12-18 15:37:45 +0530

    [Deepak]update:UC2 assignment is completed in uc1 file

--- UC1/employee_wage.js
+++ UC1/employee_wage.js
@@ -1,8 +1,32 @@
+PART_TIME = 4;
+FULL_TIME = 6;
+NO_TIME = 0;
+PER_HOUR_WAGE = 20;
 
-const x = Math.floor(Math.random()*2)
+const isPresent = Math.floor(Math.random() * 2);
 
-if(x==1){
-    console.log("Present")
-}else{
-    console.log("Absent")
+if (isPresent == 1) {
+  const x = Math.floor(Math.random() * 3);
+
+  let workhour;
+
+  switch (x) {
+    case 0:
+      workhour = 0;
+      break;
+    case 1:
+      workhour = PART_TIME;
+      break;
+    case 2:
+      workhour = FULL_TIME;
+      break;
+
+    default:
+      workhour = 0;
+      break;
+  }
+
+  console.log(workhour);
+} else {
+  console.log("Absent");
 }
